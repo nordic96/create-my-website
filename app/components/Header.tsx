20cd3d85f4b0577e689e9772875bbbf1057aadba
nordic96 <rhrlgns96@gmail.com>
2025-11-14 21:50:45 +0800

    introduced labelcontainer module

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Link from "next/link";
+import LabelContainer from "labelcontainer";
 
 const Header = () => {
+  const lsInstance = LabelContainer.getInstance();
   return (
     <header>
-      <div className="flex justify-between w-[1250px] m-auto px-16 py-8">
-        <Link href={"/"}>My Portfolio Website</Link>
+      <div className="flex justify-between w-[1250px] m-auto px-16 py-4">
+        <Link href={"/"}>{lsInstance.getLabel("header_title")}</Link>
         <div>
           <Link href={"/about"}>About</Link>
         </div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import LabelContainer from "labelcontainer";
+import { labelData } from "./constants/label";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,6 +26,8 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  LabelContainer.getInstance().setLabels(labelData);
+
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
