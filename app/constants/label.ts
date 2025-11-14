6ca5196e197779b05ce88069e7e297fe17dc2571
nordic96 <rhrlgns96@gmail.com>
2025-11-14 21:51:15 +0800

    added default labels for labelcontainer use

---
+++ app/constants/label.ts
@@ -0,0 +1,13 @@
+import { Labels } from "labelcontainer/build/types";
+
+export const labelData: Labels = {
+  GLOBAL: {
+    en: {
+      header_title: "John Doe's Portfolio Website",
+      intro_name: "John Doe",
+      intro_secondary: "Software Engineer Based in CA, US",
+      intro_desc: "Passionate, Self-Driven and Constant Learner",
+      btn_resume: "View Resume",
+    },
+  },
+};
