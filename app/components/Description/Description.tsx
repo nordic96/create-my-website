a6b98214174a10a51e05807b28baa0cc48cca932
nordic96 <rhrlgns96@gmail.com>
2025-11-15 10:16:13 +0800

    refactored header desc label components for hoc function

--- app/components/Description/Description.tsx
+++ app/components/Description/Description.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Description = (props: React.PropsWithChildren<unknown>) => {
   const { children } = props;
-  return <div className={"flex text-lg max-sm:text-base max-sm:w-full"}>{children}</div>;
+  return <div className={"flex text-md max-sm:text-base max-sm:w-full"}>{children}</div>;
 };
 
 export default Description;
--- app/components/HeaderLabel/HeaderLabel.tsx
+++ app/components/HeaderLabel/HeaderLabel.tsx
@@ -6,7 +6,7 @@ interface HeaderLabelProps {
 
 const HeaderLabel = (props: PropsWithChildren<HeaderLabelProps>) => {
   return (
-    <label className="lg:text-2xl max-sm:text-lg text-left text-shadow-sm font-semibold uppercase flex gap-1 items-center">
+    <label className="lg:text-xl max-sm:text-lg text-left text-shadow-sm font-semibold uppercase flex gap-1 items-center">
       {props.children}
     </label>
   );
