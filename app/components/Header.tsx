769d8671874cdec6e1b21749067c5cd7aad13f5e
nordic96 <rhrlgns96@gmail.com>
2025-11-15 15:11:00 +0800

    added functional banner

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,18 +1,22 @@
 import React from "react";
 import Link from "next/link";
 import LabelContainer from "labelcontainer";
+import Banner from "./Banner";
 
 const Header = () => {
   const lsInstance = LabelContainer.getInstance();
   return (
-    <header>
-      <div className="flex justify-between w-[1250px] m-auto px-16 py-4">
-        <Link href={"/"}>{lsInstance.getLabel("header_title")}</Link>
-        <div>
-          <Link href={"/about"}>About</Link>
+    <div>
+      <header>
+        <div className="flex justify-between w-[1250px] m-auto px-16 py-4">
+          <Link href={"/"}>{lsInstance.getLabel("header_title")}</Link>
+          <div>
+            <Link href={"/about"}>About</Link>
+          </div>
         </div>
-      </div>
-    </header>
+      </header>
+      <Banner msg={lsInstance.getLabel("banner_text")} />
+    </div>
   );
 };
 
