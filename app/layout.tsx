635ab73a82ec69a8fcccef205f6a0445d2c5a1ac
nordic96 <rhrlgns96@gmail.com>
2025-11-15 10:22:21 +0800

    added viewport constant for layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header";
@@ -15,6 +15,12 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 0.7,
+  userScalable: false,
+};
 
 export const metadata: Metadata = {
   title: "Create My Website",
