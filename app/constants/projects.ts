d4fa6f1848ffc9b65920464050e7675a5fb03527
nordic96 <rhrlgns96@gmail.com>
2025-11-15 11:15:08 +0800

    added placeholder project data

---
+++ app/constants/projects.ts
@@ -0,0 +1,43 @@
+import { Project } from "../components/ProjectCard/ProjectCard";
+
+export const projectData: Project[] = [
+  {
+    id: "123sadsad",
+    name: "My Portfolio Website",
+    keywords: ["React.js", "Typescript"],
+    year: "2022",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    sources: [
+      {
+        source_url: "",
+        icon_url: "/brands/github.svg",
+      },
+    ],
+  },
+  {
+    id: "214tedsad",
+    name: "Reusable Components",
+    keywords: ["Storybook", "React.js", "Typescript"],
+    year: "2021",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    sources: [
+      {
+        source_url: "",
+        icon_url: "/brands/github.svg",
+      },
+    ],
+  },
+  {
+    id: "ghffdsfdsdf23",
+    name: "Medium Blog Post",
+    keywords: ["Frontend", "SoftwareEngineering", "WebDev"],
+    year: "2025",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    sources: [
+      {
+        source_url: "",
+        icon_url: "/brands/github.svg",
+      },
+    ],
+  },
+];
